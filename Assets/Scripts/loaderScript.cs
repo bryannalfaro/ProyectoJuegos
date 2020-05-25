5eb7fe18a1d105ef7c6a477b6d8236c4eb6d987b
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-05-25 15:03:52 -0600

    manejo de transiciones de escenas y canvas menu

---
+++ Assets/Scripts/loaderScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loaderScript : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex+1));
+    }
+
+    //Coroutine
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
