10940d888a7f3d19759bec95796afd3d42ca78f7
Julio Herrera <jurhs2000@gmail.com>
2020-05-31 19:38:10 -0600

    cargando escena level1

--- Assets/Scripts/Level1Manager.cs
+++ Assets/Scripts/Level1Manager.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Level1Manager : MonoBehaviour
 {
+    public GameObject canvasUI;
     private AudioManager audioManager;
     // Start is called before the first frame update
     void Start()
     {
         audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
         audioManager.Play("InGameSound");
+        StartCoroutine(activeUI());
+    }
+
+    IEnumerator activeUI()
+    {
+        yield return new WaitForSeconds(0.75f);
+        canvasUI.SetActive(true);
     }
 
     // Update is called once per frame
